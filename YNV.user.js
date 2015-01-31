a1c6a4a2401922fa62794abec440519166184f4d
somini <somini29@yandex.com>
2015-01-31 22:37:45 +0000

    Minor cosmetic fixes
    Don't show the scrollbar, ever.

--- YNV.user.js
+++ YNV.user.js
@@ -3,13 +3,14 @@
 // @namespace   somini
 // @description Watch youtube videos on your browser's native player.
 // @include     https://www.youtube.com/watch*
-// @version     2.2
+// @version     2.2.1
 // @grant       none
 // ==/UserScript==
 
 document.addEventListener("DOMContentLoaded", function() {
   vid = document.getElementsByTagName('video')[0]; //There's a single video
   if (vid) { // is not null
+    document.body.class = ''; document.body.style = 'overflow-y: hidden;';
     document.body.innerHTML = '<video width="100%" height="100%" controls style="background: black" src="'+vid.src+'" />';
   }
 });
