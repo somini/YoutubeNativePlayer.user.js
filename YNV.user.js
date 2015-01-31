b39f56c70dc7b7e4909a4fa62f5d5ba880fa356c
somini <somini29@yandex.com>
2015-01-31 23:39:30 +0000

    Corrected className

--- YNV.user.js
+++ YNV.user.js
@@ -3,14 +3,14 @@
 // @namespace   somini
 // @description Watch youtube videos on your browser's native player.
 // @include     https://www.youtube.com/watch*
-// @version     2.2.1
+// @version     2.2.2
 // @grant       none
 // ==/UserScript==
 
 document.addEventListener("DOMContentLoaded", function() {
   vid = document.getElementsByTagName('video')[0]; //There's a single video
   if (vid) { // is not null
-    document.body.class = ''; document.body.style = 'overflow-y: hidden;';
+    document.body.className = ''; document.body.style = 'overflow-y: hidden;';
     document.body.innerHTML = '<video width="100%" height="100%" controls style="background: black" src="'+vid.src+'" />';
   }
 });
